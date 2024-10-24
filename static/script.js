386d2da1baef81bd04b56e3bff3a34d658fce389
mohamedfattouhy <78499945+mohamedfattouhy@users.noreply.github.com>
2024-10-24 22:57:43 +0200

    Update HTML/CSS and JS files to add progress bar

--- static/script.js
+++ static/script.js
@@ -52,3 +52,57 @@ document.addEventListener('DOMContentLoaded', function () {
     setupCarouselControls('#carouselDashFuel');
     setupCarouselControls('#carouselDashBike');
 });
+
+
+
+// Pop-up si le site est consulté sur mobile pour inciter à desactiver le mode sombre
+
+// Fonction pour vérifier si l'utilisateur est sur mobile
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+// Fonction pour afficher le pop-up si le mode sombre est activé
+function checkDarkMode() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && isMobileDevice()) {
+        document.getElementById('darkModePopup').style.display = 'block';
+    }
+}
+
+// // Fonction pour fermer le pop-up
+// function closePopup() {
+//     document.getElementById('darkModePopup').style.display = 'none';
+// }
+
+function closePopup() {
+    // Logique pour fermer le popup
+    const popup = document.getElementById('darkModePopup'); // Assure-toi d'avoir un élément avec cet ID
+    if (popup) {
+      popup.style.display = 'none'; // Cache le popup
+    }
+  }
+
+// Vérification au chargement de la page
+document.addEventListener('DOMContentLoaded', function() {
+    checkDarkMode();
+});
+
+
+// Gestion de la progression de la bar en fonction du scroll
+const main = document.querySelector('main'),
+  progressBar = document.querySelector('#progress');
+
+document.addEventListener('scroll', () => {
+
+  let totalHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+      ),
+      clientHeight = document.documentElement.clientHeight,
+      userScroll = window.scrollY,
+      pctScrolled = Math.round((userScroll / (totalHeight-clientHeight))*100);
+  
+  progressBar.style.width = pctScrolled + '%';
+
+});
